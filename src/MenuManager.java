444326adba82b7b1b40fb64c8ed34f86cbea5f33
user <user@DESKTOP-0DMPFMI>
2023-03-20 20:13:03 +0900

    removed hello.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,13 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add students");
+		System.out.println("2. Delete students");
+		System.out.println("3. Edit students");
+		System.out.println("4. View students");
+		System.out.println("5. Exit");
+
+	}
+
+}
