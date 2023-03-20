0badbccbf6ccf2dbc464927ec66e4d27a8eeb6a9
user <user@DESKTOP-0DMPFMI>
2023-03-20 21:31:32 +0900

    remove MenuManager and added PhoneNumberManager

--- src/PhoneNumberManager.java
+++ src/PhoneNumberManager.java
@@ -1,13 +1,11 @@
 
-public class MenuManager {
+public class PhoneNumberManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add students");
-		System.out.println("2. Delete students");
-		System.out.println("3. Edit students");
-		System.out.println("4. View students");
-		System.out.println("5. Exit");
-
+		System.out.println("1. Add phone_number");
+		System.out.println("2. Delete phone_number");
+		System.out.println("3. Edit phone_number");
+		System.out.println("4. Exit");
 	}
 
 }
