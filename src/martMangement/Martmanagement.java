1c30b9af2d91b2d364d8f2034e2d07cec201ef25
user <user@DESKTOP-0DMPFMI>
2023-03-30 15:08:09 +0900

    editproject

--- src/martMangement/Martmanagement.java
+++ src/martMangement/Martmanagement.java
@@ -9,7 +9,7 @@ public class Martmanagement {
 		Scanner input =new Scanner(System.in);
 		
 		while(num!=6) {
-			System.out.println("1.Add stocks");
+			System.out.println("1. Add stocks");
 			System.out.println("2. Delete stocks");
 			System.out.println("3. Edit stocks");
 			System.out.println("4. View stocks");
@@ -17,27 +17,57 @@ public class Martmanagement {
 			System.out.println("6. Exit");
 			System.out.println("Select one number between 1-6:");
 			num=input.nextInt();
-			
-			switch(num) {
-			case 1:System.out.print("Stock Id:");
-			int stockId = input.nextInt();
-			System.out.print("Stock Item:");
-			String stockItem = input.next();
-			System.out.print("Stock Name:");
-			String stockName = input.next();
-			break;
-			
-			case 2:
-			case 3:
-			case 4:
-				System.out.print("Stock Id:");
-				int stockId2 =input.nextInt();
+			if (num==1) {
+				addStocks();
 			}
+			else if (num==2) {
+				deleteStocks();
+			}
+			else if (num==3) {
+				editStocks();
+			}
+			else if (num==4) {
+				viewStocks();
+			}
+			else {
+				continue;
+			}
+			
 		}
 	}
+	public static void addStocks() {
+		Scanner input =new Scanner(System.in);	
 		
+		System.out.print("Stocks ID: "); 
+		int stocksID=input.nextInt();
 		
-		
+		System.out.print("Stocks Item: ");
+		String stocksItem=input.next();
+		System.out.println(stocksItem);
+
+		System.out.println("stocksName:");
+		String stocksName=input.next();
+		System.out.println(stocksName);
 	}
+	
+	
+	public static void deleteStocks() {
+		Scanner input =new Scanner(System.in);
+		System.out.print("Stocks ID: ");
+		int stocksID = input.nextInt();
+	}
+	public static void editStocks() {
+		Scanner input =new Scanner(System.in);
+		System.out.print("Stocks ID: ");
+		int stocksID = input.nextInt();
+	}
+	
+	public static void viewStocks() {
+		Scanner input =new Scanner(System.in);
+		System.out.print("Stocks ID: ");
+		int stocksID = input.nextInt();
+	}
+	
+}
 
 
