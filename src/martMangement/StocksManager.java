029fc1893f57e8172fe09cf1d7230e2f744338ba
user <user@DESKTOP-0DMPFMI>
2023-04-17 23:42:04 +0900

    edit

--- src/martMangement/StocksManager.java
+++ src/martMangement/StocksManager.java
@@ -1,16 +1,17 @@
 package martMangement;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StocksManager {
-	Stocks stocks;
+	ArrayList<Stocks> stock=new ArrayList<Stocks>();
 	Scanner input;
 	StocksManager(Scanner input){
 		this.input=input;
 	}
 
 	public void addStocks() {
-		stocks= new Stocks();
+		Stocks stocks= new Stocks();
 		System.out.print("Stocks ID: ");
 		stocks.id=input.nextInt();
 		
@@ -19,36 +20,74 @@ public class StocksManager {
 		
 		System.out.print("Stocks Name: ");
 		stocks.name=input.next();
+		stock.add(stocks);
+
 	}
 	
 	public void deleteStocks() {
 		System.out.print("Stocks ID: ");
 		int stocksID =input.nextInt();
-		if (stocks==null) {
-			System.out.println("the stocks has not been registered");
+		int index=-1;
+		for(int i=0;i<stock.size();i++) {
+			if (stock.get(i).id==stocksID) {
+				index=i;
+				break;
+			}
+		}
+		if(index>=0) {
+			stock.remove(index);
+			System.out.println("the stock "+stocksID+" is deleted");
+		}
+		
+		else {
+			System.out.println("the stock has not been registered");
 			return;
 		}
-		if (stocks.id==stocksID) {
-			stocks = null;
-			System.out.println("the stocks is deleted");
-		}
+	
 	}
 	
 	public void editStocks() {
 		System.out.print("Stocks ID: ");
 		int stocksID =input.nextInt();
+		
+		for(int i=0;i<stock.size();i++) {	
+			Stocks stocks =stock.get(i);
+
 		if (stocks.id==stocksID) {
-			stocks = null;
-			System.out.println("the stock is to be edited is"+ stocksID);
-			return;
-		}
-	}
+			int num =-1;
+			while(num!=5) {
+				System.out.println("***Stocks Management System Menu***");
+				System.out.println("1. Edit Id: ");
+				System.out.println("2. Edit Item: ");
+				System.out.println("3. Edit name: ");
+				System.out.println("4. Exit ");
+				
+				num=input.nextInt();
+				
+				if(num==1) {
+					System.out.println("Stock ID:");
+					stocks.id=input.nextInt();
+				}
+				else if(num==2) {
+					System.out.println("Stock Item:");
+					stocks.item=input.next();				}
+				else if(num==3) {
+					System.out.println("Stock Name:");
+					stocks.name=input.next();				}
+				
+				else {
+					break;
+				}//if
+			}//while
+			break;
+		}//if
+	
+	}//for
+}
 	
 	public void viewStocks() {
-		System.out.print("Stocks ID: ");
-		int stocksID =input.nextInt();
-		if (stocks.id==stocksID) {
-		stocks.printInfo();
-	}
+		for(int i=0;i<stock.size();i++) {		
+			stock.get(i).printInfo();
+		}
 }
 }
