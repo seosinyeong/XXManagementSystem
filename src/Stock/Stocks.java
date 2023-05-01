63edee904d99c568e3753c4062041537531b9a9d
user <user@DESKTOP-0DMPFMI>
2023-05-01 23:01:49 +0900

    edit a

--- src/Stock/Stocks.java
+++ src/Stock/Stocks.java
@@ -1,9 +1,12 @@
-package martMangement;
+package Stock;
+
+import java.util.Scanner;
 
 public class Stocks {
-	int id;
-	String item;
-	String name;
+	protected StockKind kind = StockKind.ConvenienceStore;
+	protected int id;
+	protected String item;
+	protected String name;
 	
 	public Stocks() {
 	}
@@ -21,9 +24,62 @@ public class Stocks {
 		this.name=name;
 	
 	}
-	
+	public StockKind getKind() {
+		return kind;
+	}
+
+
+	public void setKind(StockKind kind) {
+		this.kind = kind;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getItem() {
+		return item;
+	}
+
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public void  printInfo() {
 		System.out.println("id:"+id+"  item:"+item+"  name:"+name);
+	}
+	
+	public void getUserInput(Scanner input) {
+		System.out.print("Stocks ID: ");
+		int id=input.nextInt();
+		this.setId(id);
+		
+		System.out.print("Stocks Item: ");
+		String item=input.next();
+		this.setItem(item);
+		
+		System.out.print("Stocks Name: ");
+		String name=input.next();
+		this.setName(name);
+
 	}
 }
 
