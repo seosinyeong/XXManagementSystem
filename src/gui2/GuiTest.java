39e1aa2e19a9dad54c749585d2c37030387acba9
user <user@DESKTOP-0DMPFMI>
2023-06-03 17:30:51 +0900

    add

---
+++ src/gui2/GuiTest.java
@@ -0,0 +1,11 @@
+package gui2;
+
+public class GuiTest {
+
+	public static void main(String[] args) {
+		//MenuSelection menuselection = new MenuSelection();
+		
+		StockAdder stockadder = new StockAdder();
+	}
+
+}
