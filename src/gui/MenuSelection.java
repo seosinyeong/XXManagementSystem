0e6d83e9e61034ced31e94671c0923127cfe3108
user <user@DESKTOP-0DMPFMI>
2023-06-20 19:09:35 +0900

    add

--- src/gui/MenuSelection.java
+++ src/gui/MenuSelection.java
@@ -12,36 +12,36 @@ import listener.ButtonViewListener;
 
 public class MenuSelection extends JPanel{
 	
-	WindowFrame frame;
-	public MenuSelection(WindowFrame frame) {
-		
-		this.frame = frame;
+WindowFrame frame;
 	
+	public MenuSelection(WindowFrame frame) {
+		this.frame=frame;
+		
 		this.setLayout(new BorderLayout());
 		
-		JPanel panel1 = new JPanel();
-		JPanel panel2 = new JPanel();
-		JLabel label = new JLabel("Menu Selection");
+		JPanel panel1=new JPanel();
+		JPanel panel2=new JPanel();
+		JLabel label=new JLabel("Menu Selection");
 		
-		JButton button1 = new JButton("Add Stock");
-		JButton button2 = new JButton("View Stock");
-		JButton button3 = new JButton("Edit Stock");
-		JButton button4 = new JButton("Delete Stock");
-		JButton button5 = new JButton("Exit Program");
-
+		JButton button1=new JButton("Add Stock");
+		JButton button2=new JButton("Delete Stock");
+		JButton button3=new JButton("Edit Stock");
+		JButton button4=new JButton("View Stock");
+		JButton button5=new JButton("Exit Program");
+		
 		button1.addActionListener(new ButtonAddListener(frame));
-		button2.addActionListener(new ButtonViewListener(frame));
+		button4.addActionListener(new ButtonViewListener(frame));
 		
 		panel1.add(label);
 		panel2.add(button1);
 		panel2.add(button2);
 		panel2.add(button3);
-		panel2.add(button4);		
+		panel2.add(button4);
 		panel2.add(button5);
-
+		
 		this.add(panel1,BorderLayout.NORTH);
 		this.add(panel2,BorderLayout.CENTER);
-	
+	}
 	
 	}
-}
+
--- src/gui/StockAdder.java
+++ src/gui/StockAdder.java
@@ -7,42 +7,55 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
+import event.StockAdderCancelListener;
+import event.StockAdderListener;
+import martMangement.StockManager;
+
 public class StockAdder extends JPanel{
 	
 	WindowFrame frame;
+	StockManager stockManager;
 	
-	public StockAdder(WindowFrame frame) {
+	public StockAdder(WindowFrame frame,StockManager stockManager) {
+		this.frame=frame;
+		this.stockManager= stockManager;
 		
-		this.frame = frame;
-
-		JPanel panel = new JPanel();
+		JPanel panel=new JPanel(new SpringLayout());
 		panel.setLayout(new SpringLayout());
 		
-		JLabel labelID = new JLabel("ID: ",JLabel.TRAILING);
-		JTextField fieldID = new JTextField (10);
+		JLabel labelID=new JLabel("ID: ",JLabel.TRAILING);
+		JTextField fieldID=new JTextField(10);
 		labelID.setLabelFor(fieldID);
 		panel.add(labelID);
 		panel.add(fieldID);
 		
-		JLabel labelItem = new JLabel("Item: ",JLabel.TRAILING);
-		JTextField fieldItem = new JTextField (10);
+		JLabel labelItem=new JLabel("Item: ",JLabel.TRAILING);
+		JTextField fieldItem=new JTextField(10);
 		labelItem.setLabelFor(fieldItem);
 		panel.add(labelItem);
 		panel.add(fieldItem);
 		
-		JLabel labelName = new JLabel("Name: ",JLabel.TRAILING);
-		JTextField fieldName = new JTextField (10);
+		JLabel labelName=new JLabel("Name: ",JLabel.TRAILING);
+		JTextField fieldName=new JTextField(10);
 		labelName.setLabelFor(fieldName);
+		
+		JButton saveButton=new JButton("save");
+		saveButton.addActionListener(new StockAdderListener(fieldID,fieldItem,fieldName,stockManager));
+		
+		JButton cancelButton=new JButton("cancel");
+		cancelButton.addActionListener(new StockAdderCancelListener(frame));
+		
 		panel.add(labelName);
 		panel.add(fieldName);
 		
-		panel.add(new JButton("save"));
-		panel.add(new JButton("cancel"));
+		panel.add(saveButton);
+		panel.add(cancelButton);
 		
-		SpringUtilities.makeCompactGrid(panel,4,2,6,6,6,6);
+		SpringUtilities.makeCompactGrid(panel,4, 2, 6, 6, 6, 6);
 		
 		
 		this.add(panel);
 		this.setVisible(true);
-	}
+		
+			}
 }
